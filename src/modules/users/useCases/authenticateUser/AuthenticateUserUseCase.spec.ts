a5c8d13c5c962290e639de229fb551d547fa21f7
Angelo Filho <angelo.filho06@hotmail.com>
2023-02-07 13:27:18 -0300

    test: authenticate user use case test

--- src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,14 +1,56 @@
+import { AppError } from "../../../../shared/errors/AppError";
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
 
 let authenticateUserUseCase: AuthenticateUserUseCase;
+let createUserUseCase: CreateUserUseCase;
 let usersRepository: InMemoryUsersRepository;
+
+const test_user = {
+  name: "test",
+  email: "test@test.com",
+  password: "test_password",
+};
 
 describe("Authenticate User", () => {
   beforeAll(() => {
     usersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepository);
     authenticateUserUseCase = new AuthenticateUserUseCase(usersRepository);
   });
 
-  it("should be able to authenticate a user", async () => {});
+  it("should be able to authenticate a user", async () => {
+    await createUserUseCase.execute(test_user);
+
+    const user_and_token = await authenticateUserUseCase.execute({
+      email: test_user.email,
+      password: test_user.password,
+    });
+
+    expect(user_and_token).toHaveProperty("user");
+    expect(user_and_token).toHaveProperty("token");
+  });
+
+  it("should not be able to authenticate a user with incorrect email", () => {
+    expect(async () => {
+      await createUserUseCase.execute(test_user);
+
+      await authenticateUserUseCase.execute({
+        email: "wrong_email",
+        password: test_user.password,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to authenticate a user with incorrect password", () => {
+    expect(async () => {
+      await createUserUseCase.execute(test_user);
+
+      await authenticateUserUseCase.execute({
+        email: test_user.email,
+        password: "wrong_password",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
 });
